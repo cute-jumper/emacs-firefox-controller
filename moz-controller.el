71698c9ae7f52401cdb370a434fc5040623994ca
Ren Wenshan <wenshan@trikeapps.com>
2014-05-29 08:04:42 +1000

    Initial commit.

---
+++ moz-controller.el
@@ -0,0 +1,42 @@
+;;; moz-controller.el --- Control Firefox from Emacs
+
+;; Copyright (C) 2014 任文山 (Ren Wenshan)
+
+;; Author: 任文山 (Ren Wenshan) <renws1990 at gmail.com>
+;; URL: https://github.com/renws/emacs-moz-controller
+;; Version: 0.0.1
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; With this program and Firefox plugin `MozRepl', you can control your Firefox
+;; to scroll up, scroll down, zoom in, zoom out, switch tabs, close current
+;; tabs and etc.
+
+;;; Installation:
+
+;; Place this program in your load path and add following code.
+
+;; (require 'moz-controller)
+
+;;; Usage:
+;; TODO
+
+;;; Code:
+;; TODO
+
+(provide 'moz-controller)
+;;; moz-controller.el ends here
