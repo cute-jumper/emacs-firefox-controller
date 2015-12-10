514bd6f44b5b304b1cae42fd157d98028ddd9968
Junpeng Qiu <qjpchmail@gmail.com>
2015-12-10 15:06:49 -0500

    Update docs

--- firefox-controller.el
+++ firefox-controller.el
@@ -24,13 +24,157 @@
 
 ;;; Commentary:
 
-;; With this program and Firefox plugin `MozRepl', you can control your Firefox
-;; to scroll up, scroll down, zoom in, zoom out, switch tabs, close current
-;; tabs and etc.
+;;            __________________________________________________
 
-;;; Installation: see the Installation section in the README.org
+;;                 FIREFOX-CONTROLLER: AN IMPROVED FIREFOX
+;;                          CONTROLLER FOR EMACS!
 
-;;; Usage: see the Usage section in the README.org
+;;                               Junpeng Qiu
+;;            __________________________________________________
+
+
+;; Table of Contents
+;; _________________
+
+;; 1 Installation
+;; .. 1.1 Firefox Extension
+;; .. 1.2 Emacs Extensions
+;; 2 Intro
+;; 3 `firefox-controller-remote-mode'
+;; 4 `firefox-controller-direct-mode'
+;; 5 Acknowledgment
+
+
+
+
+
+;; 1 Installation
+;; ==============
+
+;; 1.1 Firefox Extension
+;; ~~~~~~~~~~~~~~~~~~~~~
+
+;;   Install [MozRepl] in Firefox.
+
+
+;;   [MozRepl] https://addons.mozilla.org/en-US/firefox/addon/mozrepl/
+
+
+;; 1.2 Emacs Extensions
+;; ~~~~~~~~~~~~~~~~~~~~
+
+;;   1. Install [moz.el] and [popwin-el].
+;;   2. Install this package and add it to your load path
+;;      ,----
+;;      | (add-to-list 'load-path "/path/to/emacs-firefox-controller")
+;;      | (require 'firefox-controller)
+;;      `----
+
+
+;;   [moz.el] https://github.com/bard/mozrepl/wiki/Emacs-integration
+
+;;   [popwin-el] https://github.com/m2ym/popwin-el
+
+
+;; 2 Intro
+;; =======
+
+;;   This project started as a fork of [Wenshan]'s [moz-controller], but I
+;;   ended up rewriting most of the code and chose a quite different way to
+;;   use it.
+
+;;   In `firefox-controller', we have two different modes:
+;;   1. `firefox-controller-remote-mode': This is based on the original
+;;      `moz-controller', but the number of available commands increases
+;;      from 11 to more than 30, and simpler key bindings and a better UI
+;;      are also provided.
+;;   2. `firefox-controller-direct-mode': In this mode, you can send the
+;;      keys directly to firefox.
+
+
+;;   [Wenshan] https://github.com/RenWenshan
+
+;;   [moz-controller] https://github.com/RenWenshan/emacs-moz-controller
+
+
+;; 3 `firefox-controller-remote-mode'
+;; ==================================
+
+;;   Use `M-x firefox-controller-remote-mode' to enter
+;;   `firefox-controller-remote-mode'. It is called `remote-mode' because
+;;   the keys that you pressed are handled by Emacs and Emacs will send
+;;   control commands to firefox.
+
+;;   Here is the screeshot when using `firefox-controller-remote-mode':
+;;   [https://github.com/cute-jumper/ace-pinyin/blob/master/screenshots/remote-mode.png]
+
+;;   As we can see in the screenshot, we have most of the common commands
+;;   available. For example, n to scroll down by one page, and t to open a
+;;   new tab and switch to it. You can exit the
+;;   `firefox-controller-remote-mode' by q, and you can search the web page
+;;   by pressing s, and here is the screenshot for the search mode in
+;;   `firefox-controller-remote-mode' (we are search "bibtex" in the
+;;   current web page):
+;;   [https://github.com/cute-jumper/ace-pinyin/blob/master/screenshots/search-mode.png]
+
+
+;; 4 `firefox-controller-direct-mode'
+;; ==================================
+
+;;   The limitation of `firefox-controller-remote-mode' is that under that
+;;   mode, the web page is /non-interactive/. We can only view, scroll,
+;;   switch tab, search and open another tab for a new URL. However, if you
+;;   want to jump to a link or enter some text in the input box, these
+;;   functions are not implemented. I would recommend use
+;;   `firefox-controller-direct-mode', combined with some powerful firefox
+;;   extensions such as [KeySnail] for emacs-like mouseless browsing
+;;   experience, instead of recreating some firefox extensions in
+;;   `firefox-controller-remote-mode'.
+
+;;   The use of `firefox-controller-direct-mode' is quite straightforward.
+;;   `M-x firefox-controller-direct-mode', then you can use all the key
+;;   bindings as if you are in firefox instead of Emacs, except for three
+;;   special key bindings:
+;;   1. C-g: Whenl you press C-g once, the focus of the firefox will move
+;;      to the content window, and if you press C-g twice in a row, it will
+;;      quit `firefox-controller-direct-mode'.
+;;   2. M-g: This is bound to `firefox-controller-highlight-focus', which
+;;      can show a temporary background color in the current focused
+;;      element. This command is useful since the foreground application is
+;;      Emacs, firefox won't show the current focused element(at least, we
+;;      can't see it in Plasma 5 in Linux, which is my test environment).
+;;      You can use M-g to give you a visual hint about the location of the
+;;      cursor.
+;;   3. C-z: This command switches from the current mode to
+;;      `firefox-controller-remote-mode'.
+
+;;   Here is the screenshot to use `firefox-controller-direct-mode':
+;;   [https://github.com/cute-jumper/ace-pinyin/blob/master/screenshots/direct-mode.gif]
+
+;;   Explanation: After I invoke `firefox-controller-direct-mode', I type
+;;   C-l to go to the address bar, and use M-g to highlight my current
+;;   location(which is the address bar of course). Then go to google.com,
+;;   and use M-g again to confirm the current focused element in firefox is
+;;   the seach box. After I type and search "emacs", I use KeySnail's
+;;   plugin [hok] to jump to a link and open it.
+
+
+;;   [KeySnail] https://github.com/mooz/keysnail
+
+;;   [hok] https://github.com/mooz/keysnail/raw/master/plugins/hok.ks.js
+
+
+;; 5 Acknowledgment
+;; ================
+
+;;   - [RenWenshan] for the original [moz-controller].
+
+
+;;   [RenWenshan] https://github.com/RenWenshan/
+
+;;   [moz-controller] https://github.com/RenWenshan/emacs-moz-controller
+
+
 
 ;;; Code:
 
